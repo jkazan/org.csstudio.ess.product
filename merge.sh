371009e69b7493991d5a33be31664854fcdb8c07
jkazan <johannes.k@live.com>
2019-03-18 11:23:41 +0100

    Updating changelog, splash, manifests to version

--- merge.sh
+++ merge.sh
@@ -16,7 +16,7 @@ msg="Merge master into production."
 declare -a gitcmds=(
     "git checkout production"
     "git pull origin production"
-    "asdasd"
+    "git merge master -m \"Merge master into production.\""
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout master"
@@ -28,13 +28,14 @@ git push origin
 for i in "${repos[@]}"; do
     cd ../$i/
     for k in "${gitcmds[@]}"; do
-        if [ "$k" = "asdasd" ]; then
-            git merge master -m "Merge master into production."
-            echo -e "\nRunning: git merge master -m \"Merge master into production.\""
-        else
-            echo -e "\nRunning: $k"
-            $k
-        fi
+        $k
+        # if [ "$k" = "asdasd" ]; then
+        #     git merge master -m "Merge master into production."
+        #     echo -e "\nRunning: git merge master -m \"Merge master into production.\""
+        # else
+        #     echo -e "\nRunning: $k"
+        #     $k
+        # fi
         ret_code=$?
         echo "return code: ${ret_code}"
         if [[ ${ret_code} != 0 ]]; then
