7f5c81b478eac8cc29bc629309cc6c56b67e6922
jkazan <johannes.k@live.com>
2019-03-18 11:25:43 +0100

    Updating changelog, splash, manifests to version

--- merge.sh
+++ merge.sh
@@ -16,7 +16,7 @@ msg="Merge master into production."
 declare -a gitcmds=(
     "git checkout production"
     "git pull origin production"
-    "git merge master -m \"Merge master into production.\""
+    "git merge master"
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout master"
