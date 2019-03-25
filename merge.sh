f21bd6ee272e62344efe96d87e84bc4da735c189
jkazan <johannes.k@live.com>
2019-03-25 17:36:44 +0100

    Updating changelog, splash, manifests to version

--- merge.sh
+++ merge.sh
@@ -14,7 +14,7 @@ declare -a repos=(
 declare -a gitcmds=(
     "git checkout production"
     "git pull origin production"
-    "git merge master -m '''Merge master into production.'''"
+    "git merge master -m 'Merge master into production.'"
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout master"
@@ -23,9 +23,9 @@ declare -a gitcmds=(
 git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
 git push origin
 
-for i in "${repos[@]}"; do
+for i in $(${repos[@]}); do
     cd ../$i/
-    for k in "${gitcmds[@]}"; do
+    for k in $(${gitcmds[@]}); do
         $k
         if [[ $? != 0 ]]; then
             echo "Error occurred running: $k"
