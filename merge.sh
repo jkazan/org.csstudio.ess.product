9dfa5e259079d3f474503a7a77223ee099a90dff
jkazan <johannes.k@live.com>
2019-03-26 08:44:48 +0100

    Updating changelog, splash, manifests to version

--- merge.sh
+++ merge.sh
@@ -12,11 +12,16 @@ declare -a repos=(
 )
 
 declare -a gitcmds=(
-    "git fetch origin"
     "git checkout test"
+    "git pull"
     "git checkout production"
-    "git pull origin production"
+    "git pull"
     "git merge test -m 'test message'"
+    # "git fetch origin"
+    # "git checkout test"
+    # "git checkout production"
+    # "git pull origin production"
+    # "git merge test -m 'test message'"
     "git push origin production"
     "git tag ESS-CS-Studio-$VERSION"
     "git checkout test"
