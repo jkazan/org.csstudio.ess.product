2cc3e17726c018486a5e19086df59dd1d419466a
jkazan <johannes.k@live.com>
2019-03-26 08:52:36 +0100

    Updating changelog, splash, manifests to version

--- merge.sh
+++ merge.sh
@@ -11,37 +11,21 @@ declare -a repos=(
     "org.csstudio.ess.product"
 )
 
-msg="Merge master into production."
-
-declare -a gitcmds=(
-    "git checkout production"
-    "git pull origin production"
-    "git merge master -m 'do re mi fa so la si'"
-    "git push origin production"
-    "git tag ESS-CS-Studio-$VERSION"
-    "git checkout master"
-)
-
 git commit -a -m "Updating changelog, splash, manifests to version $VERSION"
 git push origin
 
 for i in "${repos[@]}"; do
     cd ../$i/
-    for k in "${gitcmds[@]}"; do
-        echo -e "\nRunning: $k"
-        eval $k
-        # if [ "$k" = "asdasd" ]; then
-        #     git merge master -m "Merge master into production."
-        #     echo -e "\nRunning: git merge master -m \"Merge master into production.\""
-        # else
-        #     echo -e "\nRunning: $k"
-        #     $k
-        # fi
-        ret_code=$?
-        echo "return code: ${ret_code}"
-        if [[ ${ret_code} != 0 ]]; then
-            echo "ERROR occurred running: $k"
-            exit 1
-        fi
+    git checkout production \
+        && git pull origin production \
+        && git merge master -m "Merge master into production." \
+        && git push origin production \
+        && git tag ESS-CS-Studio-$VERSION \
+        && git checkout master
+
+    if [[ $? != 0 ]]; then
+        echo "An error occured"
+        exit 1
+    fi
     done
 done
